fcc890b20e91d5bcd247bba0393da59e87c20ce6
kushdilip <kushdilip@gmail.com>
2014-02-16 19:36:47 +0530

    first page complete

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -1,50 +1,60 @@
 myApp.controller('SurveyListcontroller', ['$scope', 'surveys', function ($scope, surveys) {
-	    $scope.surveys = surveys.get();
+	$scope.surveys = surveys.get();
 
-	    $scope.delete = function(index){
-	      $scope.surveys.splice(index,1);
-	    }
+	$scope.delete = function(index){
+		$scope.surveys.splice(index,1);
+	}
 }]);
 
 myApp.controller('SurveyController',['$scope','$routeParams','$location', 'surveys',
-									 function($scope, $routeParams, $location, surveys){
-	var surveyId = +$routeParams.id ;
-	
-	$scope.survey = surveys.getById(surveyId);			
-	
-	$scope.categories = [
+	function($scope, $routeParams, $location, surveys){
+		var surveyId = +$routeParams.id ;
+
+		$scope.survey = surveys.getById(surveyId);			
+
+		$scope.categories = [
 		{id:1, name:"eMail Based"},
 		{id:2, name:"Online"},
 		{id:3, name:"SMS Based"}
-	];
+		];
 
-	$scope.save = function(){
-		surveys.save($scope.survey);
-		$location.path('/surveys');
-	}
+		$scope.save = function(){
+			surveys.save($scope.survey);
+			$location.path('/surveys');
+		}
 
-	$scope.cancel = function(){
-		$location.path('/surveys');
-	}
-}])
+		$scope.cancel = function(){
+			$location.path('/surveys');
+		}
+	}])
 
 
 myApp.controller('EnterBillInfoCtrl', ['$scope', function ($scope) {
-	
-	$scope.persons = [
-		// {name:"Rahul"},
-		// {name:"Ramesh"},
-		// {name:"Amresh"}
-	];
+	$scope.persons = [];
+	$scope.items = [];
+
+
+	//Person methods
+	$scope.addPerson = function () {
+		$scope.persons.push({name:$scope.person});
+		$scope.person = '';
+	};  
 
 	$scope.deletePerson = function(index){
 		console.log(index, $scope.persons);
-	    $scope.persons.splice(index,1);
-	    console.log(index, $scope.persons);
+		$scope.persons.splice(index,1);
+		console.log(index, $scope.persons);
 	}
 
-	$scope.addPerson = function () {
-	    $scope.persons.push({name:$scope.person});
-	    $scope.person = '';
-  	};  
+	//Food Item methods
+	$scope.addItem = function () {
+		$scope.items.push({name:$scope.item});
+		$scope.item = '';
+	};  
+
+	$scope.deleteItem = function(index){
+		// console.log(index, $scope.persons);
+		$scope.items.splice(index,1);
+		// console.log(index, $scope.persons);
+	}
 }]);
